2a642ce7faf319963197064f73f62f5774bddeb2
John Bergsten <putte_dt@hotmail.se>
2016-11-25 21:06:21 +0100

    Added script to make objects disappear if they touch something with sword tag

---
+++ Assets/Scripts/RemovedBySword.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemovedBySword : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sword")
+            Destroy(this.gameObject);
+    }
+}
