a9898e79f7513ee7b703334276e41818b4bd9cb8
John Bergsten <putte_dt@hotmail.se>
2016-11-25 20:44:02 +0100

    Added prefab for falling object. Work in progress though. Also added script to start things spining

---
+++ Assets/Scripts/StartRandomSpin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartRandomSpin : MonoBehaviour {
+
+
+    // Random variable to generate our starting torque
+    System.Random r = new System.Random();
+
+    // Use this for initialization
+    void Start ()
+    {
+        GetComponent<Rigidbody>().AddTorque(new Vector3(GetRandomFloat(), GetRandomFloat(), GetRandomFloat()));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Gets individual random float between hard coded min and max values
+    float GetRandomFloat()
+    {
+        double min = 0;
+        double max = 5;
+        return (float)(min + r.NextDouble() * (max - min));
+    }
+}
