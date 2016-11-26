46c311d3efc43a670c994c4b7833591e068ce776
Andreas Arnesson <andreas_osby@hotmail.com>
2016-11-26 02:38:35 +0100

    Added color-cordinated-collision

--- Assets/Scripts/RemovedBySword.cs
+++ Assets/Scripts/RemovedBySword.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class RemovedBySword : MonoBehaviour {
 
+    private string whichHand;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        System.Random random = new System.Random();
+        int rand = random.Next(2);
+        this.whichHand = rand == 1 ? "Left" : "Right";
+        this.GetComponentInChildren<MeshRenderer>().material = Resources.Load(this.whichHand) as Material;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,7 +19,7 @@ public class RemovedBySword : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Sword")
+        if (collision.gameObject.tag == ("Sword" + this.whichHand))
             Destroy(this.gameObject);
     }
 }
